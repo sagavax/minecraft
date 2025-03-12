198a4442a14d447886099614ba679074d3b6ed23
Tomas Misura <tmisura@gmail.com>
2025-03-12 14:41:56 +0100

    changed to .txt file

--- selectors_helpers.js
+++
@@ -1,8 +0,0 @@
- document.querySelector(`.video[video-id='${videoId}'] .video_modpack_info`); // video s attributom video-id
- document.querySelector(`.video[video-id='${videoId}']`); // video s attributom video-id
- document.querySelector(`.video[video-id='840']`);//video id: 840
- document.querySelector(`.video[video-id='840'] .video_modpack_info`); //video modpack info element
- document.querySelector(`.bug[bug-id='10'] .bug_status`).innerText
- document.querySelector(`.bug[bug-id='10'] .bug_priority`).innerText
- document.querySelector(`.bug[bug-id='${bugId}'] .bug_status`)
- document.querySelector(`.bug[bug-id='${bugId}'] .bug_priority`) 
