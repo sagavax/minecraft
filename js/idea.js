3e931a739e925e526ff3970e16e0c28bf059db51
Tomas Misura <tmisura@gmail.com>
2025-03-19 19:41:36 +0100

    added form validation

---
+++ js/idea.js
@@ -0,0 +1,22 @@
+const idea_comment_new_form= document.querySelector(".idea_comment_new form" );
+const idea_comments_list = document.querySelector(".idea_comments_list");
+
+idea_comment_new_form.addEventListener("submit", function(event) {
+          event.preventDefault(); // Prevent form submission
+        var textarea = document.querySelector(".idea_comment_new form textarea");
+        
+        // Kontrola, či je textarea prázdna
+        if (textarea.value.trim() === "") {
+            alert("Please enter a comment.");
+            return;
+        }
+
+});
+
+
+idea_comments_list.addEventListener("click",function(event) {
+    if(event.target.tagName==="BUTTON" && event.target.name("delete_comm")){
+        const commentId = event.target.getAttribute("comment-id");
+        deleteIdeaComment(commentId);
+    }
+});
