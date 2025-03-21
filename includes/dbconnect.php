6663342a1bb4f328095a49c7cbda4dc53730797e
Tomas Misura <tmisura@gmail.com>
2025-03-21 19:47:48 +0100

    Update dbconnect.php

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -4,22 +4,30 @@
 
  $link = mysqli_connect("mariadb105.r6.websupport.sk", "minecraft_db", "Us6^*qb1H-", "minecraft_db", 3315);
  //$link = mysqli_connect("localhost", "root", "", "minecraft_db", 3306);
+/* 
+ $dsn = 'mysql:host=mariadb105.r6.websupport.sk;dbname=minecraft_db;port=3315';
+$username = 'minecraft_db';
+$password = 'Us6^*qb1H-';
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+);
 
- //$link = mysqli_connect("localhost", "root", "","minecraft_db");
-
-
+$pdo = new PDO($dsn, $username, $password, $options);
+ */
 
   if (!$link) {
       echo "Error: Unable to connect to MySQL." . PHP_EOL;
       echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     exit;
-  }
+  } 
 
 //
 // Setup the UTF-8 parameters:
 // * http://www.phpforum.de/forum/showthread.php?t=217877#PHP
 //
 // header('Content-type: text/html; charset=utf-8');
-mysqli_query($link,'set character set utf8;');
-mysqli_query($link,"SET NAMES `utf8`");
+/* mysqli_query($link,'set character set utf8;');
+mysqli_query($link,"SET NAMES `utf8`"); */
 ?>
