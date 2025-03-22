7e9322f87f73b493b8e6dea3e87e45fa28cf8799
Tomas Misura <tmisura@gmail.com>
2025-03-22 17:01:03 +0100

    couple of updates

--- image_save_description.php
+++ image_save_description.php
@@ -1,3 +1,20 @@
 <?php
 
+include("includes/dbconnect.php");
 
+
+$image_id = $_POST['image_id'];
+$imaget_desscription = mysqli_real_escape_string($link, $_POST['imaget_desscription']);
+
+
+$uppate_description = "update pictures set picture_desscription='$image_description' where image_id='$image_id'";
+$result = mysqli_query($link, $uppate_description) or die(mysqli_error);
+
+
+//app logi
+
+$diary_text = "Minecraft IS: Bol upravil popis obrazka s ID $image_id";
+$add_to_diary = "INSERT INTO app_log (diary_text, date_added) VALUES ('$diary_text',now())";
+$result = mysqli_query($link, $add_to_diary) or die(mysqli_error);
+
+?>
--- images.php
+++ images.php
@@ -226,6 +226,19 @@
                 <div class="progress_bar" style="width:0%"></div>
               </div>
             </div>
+            
+            <div class="image_tags_map">
+            <?php 
+                  $get_videos_tags = "SELECT a.tag_id,b.tag_name from video_tags a, tags_list b WHERE a.tag_id NOT IN (0) and a.tag_id = b.tag_id GROUP BY b.tag_name ORDER BY b.tag_name ASC";
+                  $result_tags=mysqli_query($link, $get_videos_tags);
+                  while ($row_tags = mysqli_fetch_array($result_tags)) {
+                    $tag_id = $row_tags['tag_id'];
+                    $tag_name=$row_tags['tag_name'];
+                    echo "<button type='button' class='button blue_button small_button' tag-id=$tag_id>$tag_name</button>";
+                  }
+                  ?>
+            </div>
+
 
             <div id="picture_list">
               <?php
