adca9023d648103d0323f1f04a0e1dfc4d13655f
Tomas Misura <tmisura@gmail.com>
2025-04-16 21:46:05 +0200

    refactored and fixed issues

--- modpack_images.php
+++ modpack_images.php
@@ -1,40 +1,83 @@
-  <?php include "includes/dbconnect.php";
-      include "includes/functions.php";
+<?php 
+include "includes/dbconnect.php";
+include "includes/functions.php";
 
-      $modpack_id=$_GET['modpack_id'];
+$modpack_id = $_GET['modpack_id'];
 ?>
 
-   <h3><?php echo GetModPackName($modpack_id); ?> images</h3>
-                        
-                       <div id="new_video">
-                         <div id="upload_external_image">
-                           <form action="modpack_external_image">
-                             <input type="text" autocomplete="off" name="image_name" placeholder="add image title here ..." id="upload_local_image">
-                             <input type="text" autocomplete="off" name="image_path" placeholder="add image url here .." id="upload_external_image">
-                             <button type="submit" name="submit" class="button">Upload</button>
-                           </form>
-                         </div>
-                         </div><!-- new image -->      
+<h3><?php echo GetModPackName($modpack_id); ?> images</h3>
 
+<!-- Upload Form -->
+<div id="new_image">
+    <form action="modpack_external_image" method="POST">
+        <input type="hidden" name="modpack_id" value="<?php echo htmlspecialchars($modpack_id); ?>"> 
 
+        <input 
+            type="text" 
+            name="image_name" 
+            id="upload_local_image"
+            placeholder="Add image title here..." 
+            autocomplete="off">
 
-                        <div class="base_images_list">
-                            <form action="" method="GET">
-                              <input type="text" name="search" placeholder="search string" autocomplete="off"><!--<button type="submit" class="button small_button"><i class="fa fa-search"></i></button>-->
-                            </form>
+        <input 
+            type="text" 
+            name="image_path" 
+            id="upload_external_image" 
+            placeholder="Add image URL here..." 
+            autocomplete="off">
 
-                            <!-- get list of images -->
-                            <?php
-                                $get_modpack_images = "SELECT * from modpack_images WHERE modpack_id=$modpack_id";
-                                $result = mysqli_query($link, $get_modpack_images) or die("MySQLi ERROR: ".mysqli_error($link));
-                                while($row = mysqli_fetch_array($result)){
-                                    $img_id = $row['img_id'];
-                                    $image_name = $row['image_name'];
-                                    //echo $image_name;
-                                    echo "<div class='base_image'>"; //base_image
-                                        //$root = "http://".$_SERVER["SERVER_NAME"]."/minecraft/prod/gallery/";
-                                        //echo "<img src='".$root."base_".$base_id."/".$image_name."'>";
-                                    echo "</div>"; //base image
-                                } 
-                             ?>   
-                         </div><!--base / modpack image list -->
+        <div class="image_action">
+            <button type="submit" name="submit" class="button">Upload</button>
+        </div>
+    </form>
+</div>
+
+<!-- Image List -->
+<div class="image_list">
+    <?php
+    $dir = "gallery/";
+    $itemsPerPage = 10;
+    $current_page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+    $offset = ($current_page - 1) * $itemsPerPage;
+
+    // Query obrázkov (POZOR: neprihliada na modpack_id!)
+    //$get_modpack_images = "SELECT * FROM pictures a, pictures_modpacks b, modpacks c WHERE b.modpack_id = $modpack_id AND b.modpack_id = c.modpack_id";
+    $get_modpack_images = "SELECT a.picture_id, a.picture_name, a.picture_title, a.picture_description, a.picture_path
+    FROM pictures a
+    JOIN pictures_modpacks b ON a.picture_id = b.image_id
+    WHERE b.modpack_id = $modpack_id
+    ORDER BY a.picture_id DESC";
+    //echo $get_modpack_images;
+    $result = mysqli_query($link, $get_modpack_images) or die("MySQLi ERROR: " . mysqli_error($link));
+
+    while ($row = mysqli_fetch_array($result)) {
+        $picture_id     = $row['picture_id'];
+        $picture_title  = !empty($row['picture_title']) ? $row['picture_title'] : $row['picture_name'];
+        $picture_path   = $row['picture_path'];
+
+        echo "<div class='picture' image-id='$picture_id'>";
+            echo "<div class='picture_name'>" . htmlspecialchars($picture_title) . "</div>";
+
+            // Obrázok
+            echo "<div class='pic' image-id='$picture_id'>";
+                if (!empty(parse_url($picture_path, PHP_URL_SCHEME))) {
+                    echo "<img src='" . htmlspecialchars($picture_path) . "' title='" . htmlspecialchars($picture_title) . "' loading='lazy'>";
+                } else {
+                    echo "<img src='gallery/" . htmlspecialchars($picture_path) . "' title='" . htmlspecialchars($picture_title) . "' loading='lazy'>";
+                }
+            echo "</div>"; // .pic
+
+            // Footer s akciami
+            echo "<div class='picture_footer'>";
+                echo "<div class='picture_action' image-id='$picture_id'>";
+                    echo "<button name='add_tag' type='button' class='button small_button' title='Add tag'><i class='fas fa-tag'></i></button>";
+                    echo "<button name='add_comment' type='button' class='button small_button' title='Add new comment'><i class='fa fa-comment'></i></button>";
+                    echo "<button name='view_image' type='button' class='button small_button' title='View image'><i class='fa fa-eye'></i></button>";
+                    echo "<button name='delete_image' type='button' class='button small_button' title='Delete picture'><i class='fa fa-times'></i></button>";
+                echo "</div>"; // .picture_action
+            echo "</div>"; // .picture_footer
+        echo "</div>"; // .picture
+    }
+    ?>
+</div>
+
