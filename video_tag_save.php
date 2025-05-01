19877860e578128389f0f46bb7eca3225649d58c
Tomas Misura <tmisura@gmail.com>
2025-05-01 18:16:57 +0200

    minor changes

--- video_tag_save.php
+++ video_tag_save.php
@@ -2,15 +2,11 @@
      include("includes/dbconnect.php");
      include("includes/functions.php");
 
-     var_dump($_POST);
      $video_id = $_POST['video_id'];
      $tag_id = $_POST['tag_id'];
-
-
+     
      $save_tag = "INSERT into video_tags (video_id, tag_id) VALUES($video_id, $tag_id)";
-     //echo $save_tag;
      $result=mysqli_query($link, $save_tag) or die(mysqli_error($link));
-
 
      $diary_text="bol pridany tag id <b>$tag_id<b> do videa s id <b>$tag_id<b>";
      $create_record="INSERT INTO app_log (diary_text, date_added) VALUES ('$diary_text', now())";
