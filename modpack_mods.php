d5b08157917b002bf062d3c34fb23ce74d7f4b95
Tomas Misura <tmisura@gmail.com>
2025-08-16 00:24:11 +0200

    modlist url

--- modpack_mods.php
+++ modpack_mods.php
@@ -6,42 +6,43 @@
   $modpack_id = $_GET['modpack_id'];    
  
  //list of links with mods and their description
- 
- echo "<div class='modpack_mods_urls'>";
+echo "<dict class='modpack_mods_links_wrapper'>";
+  echo "<div class='modpack_mods_urls'>";
 
-        $get_list_of_links = "SELECT * from mods_links where modpack_id = $modpack_id";
-        $result = mysqli_query($link, $get_list_of_links) or die("MySQLi ERROR: ".mysqli_error($link));
-        if (mysqli_num_rows($result) === 0) {
-            echo "<div class='no_links'>No links. Whoud u like to add some? <button type='button' name='add_link' class='button small_button' title='Add to gallery'><i class='fa fa-plus'></i></button></div>";
-        } else {
+          $get_list_of_links = "SELECT * from mods_links where modpack_id = $modpack_id";
+          $result = mysqli_query($link, $get_list_of_links) or die("MySQLi ERROR: ".mysqli_error($link));
+          if (mysqli_num_rows($result) === 0) {
+              echo "<div class='no_links'>No links. Whoud u like to add some? <button type='button' name='add_link' class='button small_button' title='Add to gallery'><i class='fa fa-plus'></i></button></div>";
+          } else {
 
-            echo "<div class='mopack_mods_url_add'><button type='button' name='add_link' class='button small_button' title='Add to gallery'><i class='fa fa-plus'></i></button></div>";   
+              echo "<div class='modpack_mods_url_add'><h4>Add new link</h4><button type='button' name='add_link' class='button small_button' title='Add to gallery'><i class='fa fa-plus'></i></button></div>";   
+              while($row = mysqli_fetch_array($result)){ 
+                  $link_id = $row['link_id'];
+                  $link_name = $row['link_name'];
+                  $link_url = $row['modpack_mods_url'];
+
+                  echo "<div class='modpack_mods_url' data-id='$link_id'><a href='$link_url' target='_blank'>$link_url</a><div class='link_name'>$link_name</div><div class='link_action'><button class='button blue_button' name='remove_link' type='button'><i class='fas fa-times'></i></button></div></div>";
+              }
+          }
+
+  echo "</div>";
+echo "</dict>";//modpack_mods_links_wrapper 
+
+  echo "<div class='modpack_mod_list'>";
+  
+    //echo "<div class='modlist_mods_title'><h3>List of mod for the modpack ".GetModPackName($modpack_id)."</h3></div>";
+        $sql="SELECT a.mod_id, a.modpack_id, b.cat_name from modpack_mods a, mods b where a.mod_id = b.cat_id and a.modpack_id=$modpack_id and a.mod_id order by cat_name ASC";
+            $result = mysqli_query($link, $sql) or die("MySQLi ERROR: ".mysqli_error($link));
             while($row = mysqli_fetch_array($result)){ 
-                $link_id = $row['link_id'];
-                $link_name = $row['link_name'];
-                $link_url = $row['modpack_mods_url'];
+            $mod_id = $row['mod_id'];
+            $mod_name = $row['cat_name'];
+              echo "<button type='button' class='button blue_button' data-id=$mod_id name='remove_mod_from_modpack'>$mod_name</buton>";
+          
+        } 
 
-                echo "<div class='modpack_mods_url' data-id='$link_id'><a href='$link_url' target='_blank'>$link_url</a><div class='link_name'>$link_name</div><div class='link_action'><button class='button blue_button' name='remove_link' type='button'><i class='fas fa-times'></i></button></div></div>";
-            }
-        }
-
- echo "</div>";
-
- echo "<div class='modpack_mod_list'>";
- 
-  //echo "<div class='modlist_mods_title'><h3>List of mod for the modpack ".GetModPackName($modpack_id)."</h3></div>";
-       $sql="SELECT a.mod_id, a.modpack_id, b.cat_name from modpack_mods a, mods b where a.mod_id = b.cat_id and a.modpack_id=$modpack_id and a.mod_id order by cat_name ASC";
-          $result = mysqli_query($link, $sql) or die("MySQLi ERROR: ".mysqli_error($link));
-          while($row = mysqli_fetch_array($result)){ 
-          $mod_id = $row['mod_id'];
-          $mod_name = $row['cat_name'];
-            echo "<button type='button' class='button blue_button' data-id=$mod_id name='remove_mod_from_modpack'>$mod_name</buton>";
-         
-       } 
-
-         echo "<button type='button' title='Add new mod into modpack' name='add_mods' class='button small_button blue_button'><i class='fa fa-plus'></i></button>";
-         echo "<button class='button blue_button' name='reload_mods' title='Reload mod list'><i class='fas fa-sync-alt'></i></button>";                
- echo "</div>";   
+          echo "<button type='button' title='Add new mod into modpack' name='add_mods' class='button small_button blue_button'><i class='fa fa-plus'></i></button>";
+          echo "<button class='button blue_button' name='reload_mods' title='Reload mod list'><i class='fas fa-sync-alt'></i></button>";                
+  echo "</div>";   
 
 
 ?>
