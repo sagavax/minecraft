70c64579a54883b4277dc8204f7c698ff4145d2e
Tomas Misura <tmisura@gmail.com>
2026-04-13 16:07:31 +0200

    added title in button galllery

--- image.php
+++ image.php
@@ -63,7 +63,7 @@
 
                               echo "<div class='picture_info'>";
                                   echo "<div class='picture_modpacks'>".GetImageModpack($picture_id).GetImageGallery($picture_id)." </div>";
-                                  echo "<div class='image_description' data-placeholder='image descriptpion. click / tap here to put some wideo description here ...' title='Image description'>$picture_description<button name='save_description' type='button' class='button small_button'><i class='fa fa-save'></i></button></div>";  
+                                  echo "<div class='image_description' title='Image description'>$picture_description<button name='save_description' type='button' class='button small_button'><i class='fa fa-save'></i></button></div>";  
                                   echo "<div class='image_detail_tags' tag-list='".GetImageTagListArray($picture_id)."'>".GetImageTagList($picture_id)."</div>";  
                                
                               echo "</div>"; //picture_info                                            
